5f674a7c5760c8f360124961b8660a8b9a69b2c5
justin <justin.j@neoito.com>
2021-04-18 21:57:22 +0530

    added image bg

--- src/App.js
+++ src/App.js
@@ -6,8 +6,16 @@ function App() {
   return (
     <div className="App">
       <div className="app-inner">
-        <DatePicker />
+        <div>
+          <h3 className="heading">Save water</h3>
+        </div>
+        <div className="margin-T-20 margin-B-20 react-date-select">React Day Select</div>
+        <DatePicker 
+        />
       </div>
+      <footer>
+        <div className="footer-div">A drop of water is worth more than a sack of gold to a thirsty man</div>
+      </footer>
     </div>
   );
 }
